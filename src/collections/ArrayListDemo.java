4b52a85f94b09d9ddc1b09da0882cf12ed8460fb
Dhiraj Begani <43821291+dhirajbegani@users.noreply.github.com>
2019-10-28 01:03:00 +0800

    Array List demo.

--- src/collections/ArrayListDemo.java
+++ src/collections/ArrayListDemo.java
@@ -1,11 +1,13 @@
 package collections;
 
 import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Collections;
 import java.util.Iterator;
+import java.util.LinkedHashSet;
 import java.util.List;
+import java.util.Set;
 import java.util.function.Predicate;
-
-
 
 public class ArrayListDemo {
 
@@ -13,46 +15,71 @@ public class ArrayListDemo {
 		// TODO Auto-generated method stub
 
 		List<String> stringList = new ArrayList<>();
-		
-		stringList.add("Derik");		
-		stringList.add("Kane");		
+
+		stringList.add("Derik");
+		stringList.add("Kane");
 		stringList.add("Vector");
-		
-		//Iterate using forEach
+
+		// Iterate using forEach
 		stringList.forEach(a -> {
-			a="Name is " + a;
-			System.out.println(a);});
-		
-		//Iterate using iterator
+			a = "Name is " + a;
+			System.out.println(a);
+		});
+
+		// Iterate using iterator
 		Iterator<String> itr = stringList.iterator();
-		
+
 		while (itr.hasNext()) {
 			System.out.println(itr.next());
 		}
-		
+
 		SamplePredicate<String> filter = new SamplePredicate<String>();
-		
+
 		stringList.removeIf(filter);
+
+		System.out.println("Array lsit after removing all elements that contains \"K\" " );
+		System.out.println(stringList);
+
+		// Add duplicates in Arraylist.
+		stringList.add("Kane");
+		stringList.add("Derik");
+		stringList.add("Vector");
+
+		System.out.println("Array List with duplicates "); 
+		System.out.println(stringList);
+
 		
-		//Iterate using forEach
-				stringList.forEach(a -> {
-					a="Name is " + a;
-					System.out.println(a);});
-				
+		Set <String> stringListWithoutDuplicates = new LinkedHashSet<String>(stringList);
+		
+		//Remove all elements from array list
+		stringList.clear();
+		
+		stringList.addAll(stringListWithoutDuplicates);
+		System.out.println("Array List without duplicates (Ordering guranteed as LinkedHashSet is used)");
+		System.out.println(stringList);
+		
+		Collection<String> newUnmodifiableList = Collections.unmodifiableCollection(stringList);
+		
+		try {
+		newUnmodifiableList.add("Any Name");
+		} catch(UnsupportedOperationException e) {
+		System.out.println("Add operation not allowed");
+		}
+		
+		System.out.println(newUnmodifiableList);
 	}
 
-	
 }
 
-class SamplePredicate<T> implements Predicate<T>{
-	
+class SamplePredicate<T> implements Predicate<T> {
+
 	public boolean test(T t) {
-		
-		if(t.toString().contains("K")) {
-		
-		return true;
+
+		if (t.toString().contains("K")) {
+
+			return true;
 		}
 		return false;
 	}
-	
+
 }
