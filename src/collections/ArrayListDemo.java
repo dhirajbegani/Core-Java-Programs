9a4c815ffcdd74b4981a2a198c0b7d851aa83a02
Dhiraj Begani <43821291+dhirajbegani@users.noreply.github.com>
2019-11-02 19:08:27 +0800

    Test Cases
    Added new test cases

--- src/collections/ArrayListDemo.java
+++ src/collections/ArrayListDemo.java
@@ -7,24 +7,28 @@ import java.util.Iterator;
 import java.util.LinkedHashSet;
 import java.util.List;
 import java.util.Set;
-import java.util.function.Predicate;
 
 public class ArrayListDemo {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+	List<String> stringList = new ArrayList<>();
 
-		List<String> stringList = new ArrayList<>();
-
-		stringList.add("Derik");
-		stringList.add("Kane");
-		stringList.add("Vector");
+	private void iterateUsingForEach(List<String> list) {
 
 		// Iterate using forEach
-		stringList.forEach(a -> {
+		list.forEach(a -> {
 			a = "Name is " + a;
 			System.out.println(a);
 		});
+
+	}
+
+	private static void addThreeElements(ArrayListDemo obj) {
+		obj.stringList.add("Derik");
+		obj.stringList.add("Kane");
+		obj.stringList.add("Vector");
+	}
+
+	private void iterateUsingIterator(List<String> list) {
 
 		// Iterate using iterator
 		Iterator<String> itr = stringList.iterator();
@@ -32,54 +36,41 @@ public class ArrayListDemo {
 		while (itr.hasNext()) {
 			System.out.println(itr.next());
 		}
-
-		SamplePredicate<String> filter = new SamplePredicate<String>();
-
-		stringList.removeIf(filter);
-
-		System.out.println("Array lsit after removing all elements that contains \"K\" " );
-		System.out.println(stringList);
-
-		// Add duplicates in Arraylist.
-		stringList.add("Kane");
-		stringList.add("Derik");
-		stringList.add("Vector");
-
-		System.out.println("Array List with duplicates "); 
-		System.out.println(stringList);
-
-		
-		Set <String> stringListWithoutDuplicates = new LinkedHashSet<String>(stringList);
-		
-		//Remove all elements from array list
-		stringList.clear();
-		
-		stringList.addAll(stringListWithoutDuplicates);
-		System.out.println("Array List without duplicates (Ordering guranteed as LinkedHashSet is used)");
-		System.out.println(stringList);
-		
-		Collection<String> newUnmodifiableList = Collections.unmodifiableCollection(stringList);
-		
-		try {
-		newUnmodifiableList.add("Any Name");
-		} catch(UnsupportedOperationException e) {
-		System.out.println("Add operation not allowed");
-		}
-		
-		System.out.println(newUnmodifiableList);
 	}
 
-}
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
 
-class SamplePredicate<T> implements Predicate<T> {
+		ArrayListDemo obj = new ArrayListDemo();
+		addThreeElements(obj);
 
-	public boolean test(T t) {
+		obj.iterateUsingForEach(obj.stringList);
+		obj.iterateUsingIterator(obj.stringList);
 
-		if (t.toString().contains("K")) {
+		obj.stringList.removeIf(a -> a.toString().contains("K"));
 
-			return true;
+		System.out.println("Array lsit after removing all elements that contains \"K\" ");
+		System.out.println(obj.stringList);
+
+		// Add duplicates in Arraylist.
+		addThreeElements(obj);
+
+		System.out.println("Array List with duplicates ");
+		System.out.println(obj.stringList);
+
+		Set<String> stringListWithoutDuplicates = new LinkedHashSet<String>(obj.stringList);
+		
+		System.out.println("Array List without duplicates (Ordering guranteed as LinkedHashSet is used)");
+		System.out.println(stringListWithoutDuplicates);
+
+		Collection<String> newUnmodifiableList = Collections.unmodifiableCollection(obj.stringList);
+
+		try {
+			newUnmodifiableList.add("Any Name");
+		} catch (UnsupportedOperationException e) {
+			System.out.println("Add operation not allowed");
 		}
-		return false;
+
 	}
 
 }
--- src/core/RainWaterHarvestingCount.java
+++ src/core/RainWaterHarvestingCount.java
@@ -1,18 +1,8 @@
 package core;
 
-import static org.junit.Assert.assertEquals;
-
-import org.junit.Test;
-
 public class RainWaterHarvestingCount {
 
-	@Test
-	public void successWhenCountIsSeven() {
-		int[] input = { 0, 0, 0, 2, 0, 0, 1, 3, 0, 2, 0, 0, 0 };
-		assertEquals(7, findWaterHarvested(input));
-	}
-
-	private int findWaterHarvested(int[] inputBlocks) {
+	public int findWaterHarvested(int[] inputBlocks) {
 
 		int inputLength = inputBlocks.length;
 		int leftMax[] = new int[inputLength];
